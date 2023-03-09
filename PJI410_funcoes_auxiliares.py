8277bf9dd11f18ba53fb5df1836bba322e4e16d9
Frederico Lohmann Jr <flohmannjr@yahoo.com>
2023-03-09 11:09:09 -0300

    Update PJI410_funcoes_auxiliares.py

--- PJI410_funcoes_auxiliares.py
+++ PJI410_funcoes_auxiliares.py
@@ -39,20 +39,20 @@ ITERACOES = 500
 
 # https://stackoverflow.com/a/66343730
 
-# def set_seeds(seed):
-#     os.environ['PYTHONHASHSEED'] = str(seed)
-#     random.seed(seed)
-#     tf.random.set_seed(seed)
-#     np.random.seed(seed)
+def set_seeds(seed):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
 
-# def set_global_determinism(seed):
-#     set_seeds(seed=seed)
+def set_global_determinism(seed):
+    set_seeds(seed=seed)
 
-#     os.environ['TF_DETERMINISTIC_OPS'] = '1'
-#     os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
     
-#     tf.config.threading.set_inter_op_parallelism_threads(1)
-#     tf.config.threading.set_intra_op_parallelism_threads(1)
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(1)
 
 # set_global_determinism(seed=SEMENTE)
 
